69f32cd312cc98677f8e7f4c30522ae65525179a
smtyashe <smtyashe@gmail.com>
2023-01-27 16:11:01 +0200

    Made some changes

--- src/main/java/org/page/SearchResultsPage.java
+++ src/main/java/org/page/SearchResultsPage.java
@@ -1,10 +1,59 @@
 package org.page;
 
-public class SearchResultsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class SearchResultsPage extends BasePage {
 
-    @FindBy(name = "search")
-    WebElement searchField;
+    WebElement searchProduct = driver.findElement(By.name("search"));
+    WebElement clickSearchIcon = driver.findElement(By.className("button search-btn search-icon"));
 
+    WebElement addItemToCart = driver.findElement(By.xpath("//*[@id=\"90463774\"]/div/div[4]/div/button"));
+
+    WebElement viewCart = driver.findElement(By.xpath("//*[@id=\"shopfront-app\"]/div[2]/div/div/div[2]/div/div[2]/div/div/div/div/button"));
+
+    WebElement checkQty = driver.findElement(By.xpath("//*[@id=\"body\"]/div[4]/div[1]/div/div/div[2]/div[1]/div/div[1]/div/div/div/article/div/div[2]/div[2]/div/div[1]"));
+
+    WebElement trashCanVisible = driver.findElement(By.xpath("//*[@id=\"shopfront-app\"]/div[2]/div/div/div[2]/div/div[2]/div/div/div/div/button/div[1]/svg"));
+
+    public SearchResultsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public SearchResultsPage SearchCriteria(String searchCriteria) {
+        searchProduct.clear();
+        searchProduct.sendKeys(searchCriteria);
+        clickSearchIcon.click();
+
+        return new SearchResultsPage(driver);
+
+    }
+
+    public void addProductToCart() {
+        addItemToCart.click();
+    }
+
+    public void viewProducInoCart() {
+        addItemToCart.click();
+    }
+
+    public boolean isQtyMoreThanZero() {
+
+        if (checkQty.equals(0)) {
+
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isTrashCanVisible() {
+
+        if (checkQty.isSelected()) {
+
+            return true;
+        }
+        return false;
+    }
 
 }
